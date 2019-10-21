07d16c237a4a6d14e4975eb9046bfe8dd15db632
carneirofc <claudiofcarneiro@hotmail.com>
2019-10-21 10:54:45 -0300

    database: Fix initialization issues, unit mon etc ...

--- iocBoot/iocpowerMeter/st.cmd
+++ iocBoot/iocpowerMeter/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/powerMeter
 
-## You may have to change powerMeter to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -10,16 +7,13 @@ cd "${TOP}"
 epicsEnvSet("STREAM_PROTOCOL_PATH", "${TOP}/db")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "1073676294")
 
-## Register all support components
 dbLoadDatabase "dbd/powerMeter.dbd"
 powerMeter_registerRecordDeviceDriver pdbbase
 
 drvAsynIPPortConfigure("L0", "unix://${TOP}/U2021XAVisaInterface/socket-1")
-#drvAsynIPPortConfigure("L1", "unix://${TOP}/U2021XAVisaInterface/socket-2")
 
-## Load record instances
-dbLoadRecords("db/devU2021XA.db","P=RA-RF:PowerSensor1,R=:,PORT=L0,A=0")
-#dbLoadRecords("db/devU2021XA.db","P=RA-RF:PowerSensor2,R=:,PORT=L1,A=0")
+dbLoadRecords("db/devU2021XA.db", "P=RA-RF:PowerSensor1,R=:,PORT=L0,A=0")
+dbLoadRecords("db/debug.db",      "P=RA-RF:PowerSensor1,R=:,PORT=L0,A=0")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
